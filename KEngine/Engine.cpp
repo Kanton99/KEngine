58e09317c0de230f4c4f7bd290e4b5bbf5e1649f
Kanton99 <avolkov99@gmail.com>
2024-02-23 11:15:43 +0100

    Added external libraries for new vulkan engine

---
+++ KEngine/Engine.cpp
@@ -0,0 +1,14 @@
+#include "Engine.h"
+
+#include <SDL.h>
+#include <SDL_vulkan.h>
+
+#include "vk_initializers.h"
+#include "vk_types.h"
+
+#include <chrono>
+#include <thread>
+Engine& Engine::Get()
+{
+    // TODO: inserire l'istruzione return qui
+}
