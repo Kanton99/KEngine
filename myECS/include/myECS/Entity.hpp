73f066e767319fbec601e05ed51674ee6dad55fd
Kanton99 <avolkov99@gmail.com>
2025-07-07 17:29:51 +0200

    Small ECS update

--- myECS/include/myECS/Entity.hpp
+++ myECS/include/myECS/Entity.hpp
@@ -1,15 +1,10 @@
 #ifndef KENGINE_ENTITY_HPP
 #define KENGINE_ENTITY_HPP
 #include <string>
-// #include <stduuid/uuids.h> TODO fix uuid lib issue
+#include <stduuid/uuids.h> //TODO fix uuid lib issue
 
 namespace KEngine::myECS {
-class Entity {
-public: // Members
-	std::string name;
-	// uuids::uuid id;
-	uint32_t id;
-};
+	uuid Enitity;
 } // namespace KEngine::myECS
 
 #endif
