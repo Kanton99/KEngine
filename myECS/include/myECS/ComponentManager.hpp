03d29e0c51655a5569c8fe5f3507bbc4308baa2d
Kanton99 <avolkov99@gmail.com>
2025-08-14 13:32:20 +0200

    change templated class to inline definition

--- myECS/include/myECS/ComponentManager.hpp
+++ myECS/include/myECS/ComponentManager.hpp
@@ -16,12 +16,29 @@ template <class C> class ComponentsManager : public ComponentsManagerBase {
 	ComponentsManager(ComponentsManager &) = delete;
 	ComponentsManager &operator=(const ComponentsManager &) = delete;
 	ComponentsManager &operator=(ComponentsManager &) = delete;
-	static std::shared_ptr<ComponentsManager> get();
+	inline static std::shared_ptr<ComponentsManager> get() {
+		static std::shared_ptr<ComponentsManager<C>> instance = std::make_shared<ComponentsManager>();
+		return instance;
+	}
 
 	/**
 	 *	Create a component of type C to entity
 	 */
-	void addComponent(EntityId entity);
+	inline void addComponent(EntityId entity) {
+		this->m_components.push_back(new C());
+		int size = this->m_components.size();
+		this->m_entity_comp_reference[entity] = size;
+		this->m_comp_entity_reference[size] = entity;
+	}
+	/**
+	 *	Add a component of type C to entity
+	 */
+	inline void addComponent(EntityId entity, C *component) {
+		this->m_components.push_back(component);
+		int size = this->m_components.size();
+		this->m_entity_comp_reference[entity] = size;
+		this->m_comp_entity_reference[size] = entity;
+	}
 
 	/**
 	 * Revome a component of type C from entity
