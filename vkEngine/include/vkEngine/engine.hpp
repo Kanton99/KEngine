a6fb5eee363b6449c99a43b192f90178b9418c35
Kanton99 <avolkov99@gmail.com>
2026-04-08 16:35:18 +0200

    validation and extension support check

--- vkEngine/include/vkEngine/engine.hpp
+++ vkEngine/include/vkEngine/engine.hpp
@@ -3,11 +3,17 @@
 #include "cleanupStruct.hpp"
 #include <SDL3/SDL_video.h>
 #include <memory>
+#include <vector>
 #define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
 #define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
 #include <vulkan/vulkan.hpp>
 VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
 namespace vkEngine {
+#ifdef NDEBUG
+constexpr bool enableValidationLayers = false;
+#else
+constexpr bool enableValidationLayers = true;
+#endif
 class vkEngine {
   public:
 	vkEngine(std::shared_ptr<SDL_Window> window);
@@ -25,10 +31,14 @@ class vkEngine {
 
   private:
 	void _createInstance();
+	void _checkExtensions(std::vector<const char *> extensions);
+	void _checkLayers(std::vector<char const *> layers);
+	void _createDevice();
 
   private:
 	vk::Instance _instance;
 	std::shared_ptr<SDL_Window> _window;
+	const std::vector<char const *> _validationLayers = {"VK_LAYER_KHRONOS_validation"};
 
 	std::unique_ptr<CleanupQueue> _cleanupQueue;
 };
--- vkEngine/src/engine.cpp
+++ vkEngine/src/engine.cpp
@@ -1,9 +1,14 @@
 #include "SDL3/SDL_error.h"
 #include "SDL3/SDL_vulkan.h"
+#include <algorithm>
 #include <cstdint>
+#include <cstring>
 #include <iostream>
 #include <memory>
+#include <stdexcept>
+#include <vector>
 #include <vkEngine/engine.hpp>
+#include <vulkan/vulkan_funcs.hpp>
 #include <vulkan/vulkan_hpp_macros.hpp>
 
 namespace vkEngine {
@@ -11,7 +16,10 @@ vkEngine::vkEngine(std::shared_ptr<SDL_Window> window) :
 	_window(window),
 	_cleanupQueue(std::make_unique<CleanupQueue>()) {}
 
-void vkEngine::init() { this->_createInstance(); }
+void vkEngine::init() {
+	this->_createInstance();
+	std::cout << "Rendering engine initialization complete\n";
+}
 void vkEngine::draw() {}
 void vkEngine::cleanup() { this->_cleanupQueue->flush(); }
 
@@ -26,22 +34,58 @@ void vkEngine::_createInstance() {
 										  .apiVersion = vk::ApiVersion14};
 
 	uint32_t extensionCount;
-	auto extensions = SDL_Vulkan_GetInstanceExtensions(&extensionCount);
-	if (!extensions) {
+	auto extensionsC = SDL_Vulkan_GetInstanceExtensions(&extensionCount);
+	if (!extensionsC) {
 		std::cerr << "Failed to get window extensions: " << SDL_GetError() << std::endl;
 	}
+	std::vector extensions(extensionsC, extensionsC + extensionCount);
+#ifndef NDEBUG
 	std::cout << "Number of extensions: " << extensionCount << std::endl;
 	for (int i = 0; i < extensionCount; i++) {
 		auto extension = extensions[i];
 		std::cout << extension << std::endl;
 	}
+
+	this->_checkLayers(this->_validationLayers);
+#endif
+	this->_checkExtensions(extensions);
+
 	vk::InstanceCreateInfo createInfo{.pApplicationInfo = &appInfo,
 									  .enabledExtensionCount = extensionCount,
-									  .ppEnabledExtensionNames = extensions};
+									  .ppEnabledExtensionNames = extensions.data()};
 
 	this->_instance = vk::createInstance(createInfo);
 	if constexpr (VULKAN_HPP_DISPATCH_LOADER_DYNAMIC == 1) {
 		VULKAN_HPP_DEFAULT_DISPATCHER.init(this->_instance);
 	}
 }
+
+void vkEngine::_checkExtensions(std::vector<const char *> extensions) {
+	std::cout << "Check required extensions support\n";
+	auto extensionProperties = vk::enumerateInstanceExtensionProperties();
+	auto unsupportedPropertyIt =
+		std::ranges::find_if(extensions, [&extensionProperties](auto const &requiredExtension) {
+			return std::ranges::none_of(extensionProperties, [requiredExtension](auto const &extensionProperty) {
+				return strcmp(extensionProperty.extensionName, requiredExtension) == 0;
+			});
+		});
+	if (unsupportedPropertyIt != extensions.end()) {
+		throw std::runtime_error("Required extension not supported: " + std::string(*unsupportedPropertyIt));
+	}
+}
+
+void vkEngine::_checkLayers(std::vector<char const *> layers) {
+	std::cout << "Check required validation layers support\n";
+	auto instanceLayerProperties = vk::enumerateInstanceLayerProperties();
+	auto unsupportedLayerIt = std::ranges::find_if(layers, [&instanceLayerProperties](auto const &layer) {
+		return std::ranges::none_of(instanceLayerProperties, [layer](auto const &layerProperty) {
+			return strcmp(layerProperty.layerName, layer) == 0;
+		});
+	});
+	if (unsupportedLayerIt != layers.end()) {
+		throw std::runtime_error("Required layer not supported: " + std::string(*unsupportedLayerIt));
+	}
+}
+
+void vkEngine::_createDevice() {}
 } // namespace vkEngine
